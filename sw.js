e0e205d9d95db95142a09faca20ca56c86504cb4
tomdivoy <tom.divoy@gmail.com>
2026-04-14 14:31:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE = "fittrack-v2";
-const ASSETS = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
