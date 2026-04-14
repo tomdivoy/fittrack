c69c8e37235fb04767fe7a73a07fd67aa867eda9
tomdivoy <tom.divoy@gmail.com>
2026-04-14 14:27:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fittrack-v1";
+const CACHE = "fittrack-v2";
 const ASSETS = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
